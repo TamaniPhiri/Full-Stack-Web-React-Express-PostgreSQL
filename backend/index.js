fd6bdf51e0a6622d4bc6dba3c221295fc55abde2
Tamani Phiri <tamanigabriel0@gmail.com>
2023-06-15 19:07:56 +0200

    success requests

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,32 @@ const PORT=8080;
 app.use(express.json());
 app.use(cors());
 
+//get all users from database
+app.get('/users', async(req, res)=>{
+    try {
+        const allUsers= await pool.query(
+            "SELECT * FROM users"
+        );
+        res.json(allUsers.rows);
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+//get a specific user from database
+app.get('/users/:id', async(req, res)=>{
+    const {id} =req.params;
+    try {
+        const user = await pool.query(
+            "SELECT * FROM users WHERE id =$1",
+            [id]
+        );
+        res.json(user.rows[0]);
+    } catch (error) {
+        console.log(error);
+    }
+});
+
 //post a username and email to the database
 app.post("/user",async(req, res)=>{
     try {
@@ -23,7 +49,7 @@ app.post("/user",async(req, res)=>{
 })
 
 app.get('/',(req,res) =>{
-    res.send('Welcome to our express server please buy me a coffee 😎');
+    res.send('Welcome to our express server, please buy me a coffee 😎');
 });
 
 app.listen(PORT,() =>{
