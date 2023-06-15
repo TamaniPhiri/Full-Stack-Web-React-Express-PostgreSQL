5cf5519dde7b5d5fdd66f5585224cb9b50d5ca64
Tamani Phiri <tamanigabriel0@gmail.com>
2023-06-16 00:05:18 +0200

    simple error handling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,41 +4,51 @@ import axios from "axios";
 
 function App() {
   const [getData, setGetData] = useState([]);
-  const [data, setData] = useState({
+  const [error, setError] = useState(false);
+  const [postData, setPostData] = useState({
     names: "",
     email: "",
   });
 
   const handleChange = (e) => {
-    const value=e.target.value;
-    setData({
-      ...data,
-      [e.target.name]: value
+    const value = e.target.value;
+    setPostData({
+      ...postData,
+      [e.target.name]: value,
     });
   };
 
-  const handleSubmit=(e)=>{
+  const handleSubmit = (e) => {
     e.preventDefault();
-    const userData={
-      names:data.names,
-      email:data.email.trim()
+    const userData = {
+      names: postData.names,
+      email: postData.email.trim(),
     }
-    if(data.names===""||data.email===""){
-      alert("Please fill out name and email")
-    }
-    else{
-      axios.post("http://localhost:8080/user",userData)
-      .then((response)=>{
-        console.log(response.status,response.data.token);
-      });
-      alert("Success! Please refresh your browser to see the new user")
+    
+    if (postData.names === "" || postData.email === "") {
+      alert("Please fill out name and email");
+    } else {
+      try {
+        axios.post("http://localhost:8080/user", userData).then((response) => {
+          console.log(response.status, response.data.token);
+        });
+        alert("Success! Please refresh your browser to see the new user");
+      } catch (error) {
+        console.log(error);
+        setError(true);
+      }
     }
   };
 
   useEffect(() => {
     (async () => {
-      const result = await axios.get("http://localhost:8080/users");
-      setGetData(result.data);
+      try {
+        const result = await axios.get("http://localhost:8080/users");
+        setGetData(result.data);
+      } catch (error) {
+        setError(true);
+        console.log(error);
+      }
     })();
   }, []);
 
@@ -47,21 +57,25 @@ function App() {
       <div>
         <div>
           <form onSubmit={handleSubmit}>
-            <input type="email"
+            <input
+              type="email"
               placeholder="Enter your email"
-              value={data.email}
+              value={postData.email}
               name="email"
               onChange={handleChange}
-             />
-             <input type="text"
+            />
+            <input
+              type="text"
               placeholder="Enter full name"
-              value={data.names}
+              value={postData.names}
               name="names"
               onChange={handleChange}
-             />
-             <button type="submit">Post</button>
+            />
+            <button type="submit">Post</button>
           </form>
+          <div>{error && <p>Something went wrong 😥</p>}</div>
         </div>
+
         {getData.map((results) => (
           <div key={results.id}>
             <h1>{results.names}</h1>
